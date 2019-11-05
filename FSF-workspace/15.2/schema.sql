3ea05be98e081786e08ab92d67c8960957349c6d
Sean Richter <sean.richter04@outlook.com>
2019-11-04 18:25:44 -0600

    Initial Commit

---
+++ FSF-workspace/15.2/schema.sql
@@ -0,0 +1,7 @@
+use  todolist;
+insert into todos(text, complete,createdAt, updatedAt)
+values
+("example one", false, NOW(),  NOW());
+insert into todos(text, complete, createdAt, updatedAt)
+values
+("example two", false, NOW(), NOW());
