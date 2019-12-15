66ccef8c9dae192c23cd0ea561c6dfd241ed984d
Sean Richter <sean.richter04@outlook.com>
2019-12-15 15:06:20 -0600

    Updates

---
+++ precache-manifest.408c8bc76fe656d5a2c6393fea4e6316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61fbaadf154fa7357789f5004e0abb77",
+    "url": "/google-books/index.html"
+  },
+  {
+    "revision": "3a452d78873178d5fdd5",
+    "url": "/google-books/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f5699ecb0673ac6c00cd",
+    "url": "/google-books/static/js/2.dd0b66d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/google-books/static/js/2.dd0b66d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a452d78873178d5fdd5",
+    "url": "/google-books/static/js/main.461eb277.chunk.js"
+  },
+  {
+    "revision": "2dc5715b69970854ca58",
+    "url": "/google-books/static/js/runtime-main.515c9035.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-books/static/media/logo.5d5d9eef.svg"
+  }
+]);
